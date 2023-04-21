5ea2d193a1314608b51999a8389349891669afd2
Uneku Ogbaje <unekuogbaje@gmail.com>
2023-04-21 08:16:45 +0100

    build

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -64,7 +64,7 @@ const SignInForm = () => {
   return (
   <Fragment>
     <div className="flex items-center justify-center relative min-h-screen">
-      <div className="flex-column h-[550px] w-[450px] shadow-2xl shadow-[#003ECB] bg-white rounded-xl transform duration-200 hover:scale-110 m-auto">
+      <div className="flex-column h-[550px] w-[450px] shadow-lg shadow-[#003ECB] bg-white rounded-xl transform duration-200 hover:scale-110 m-auto">
          <h2 className='text-center text-slate-600 text-xl font-semibold m-6'> Sign in</h2>
        <form
         className="mb-4 md:flex md:flex-wrap md:justify-between m-6"
@@ -100,7 +100,7 @@ const SignInForm = () => {
              className="transform duration-200 hover:scale-110 relative">
              <Button 
              type="submit"
-             className='flex ml-auto bg-[#003ECB] hover:bg-blue-900 text-mainClr rounded-full shadow-lg w-[50%] h-10 text-[16px] mt-[38px] p-[16px 20px] items-center shadow-[#2f3782]  font-bold border-[#2f3782] cursor-pointer'>
+             className='flex ml-auto bg-[#003ECB] hover:bg-blue-900 text-mainClr rounded-full shadow-md w-[50%] h-10 text-[16px] mt-[38px] p-[16px 20px] items-center shadow-[#2f3782]  font-bold border-[#2f3782] cursor-pointer'>
               <h1 className='ml-auto uppercase'> log in </h1>
                <ChevronRight 
                 className="text-mainClr ml-auto"
@@ -113,7 +113,7 @@ const SignInForm = () => {
              <Button 
               type="button" 
               onClick={signInWithGoogle}
-              className='flex ml-auto bg-[#003ECB] hover:bg-blue-900 text-mainClr rounded-full shadow-lg w-[80%] h-10 text-[16px] mt-[38px] p-[16px 20px] items-center shadow-[#2f3782]  font-bold border-[#2f3782] cursor-pointer'>
+              className='flex ml-auto bg-[#003ECB] hover:bg-blue-900 text-mainClr rounded-full shadow-md w-[80%] h-10 text-[16px] mt-[38px] p-[16px 20px] items-center shadow-[#2f3782]  font-bold border-[#2f3782] cursor-pointer'>
               <h1 className='ml-auto uppercase'> log in with google </h1>
                <ChevronRight 
                className="text-mainClr ml-auto"
