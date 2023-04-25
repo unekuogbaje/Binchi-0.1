1196d2491967e0a2a807949d50738b1f88681fb4
Uneku Ogbaje <unekuogbaje@gmail.com>
2023-04-25 14:54:51 +0100

    made content responsive

--- src/routes/browse/browse.jsx
+++ src/routes/browse/browse.jsx
@@ -6,7 +6,7 @@ import ProductCard from "../../components/product-card/product-card";
 const Browse = () => {
     const { products } = useContext(ProductsContext)
     return (
-     <div className="grid grid-cols-4 gap-x-5 gap-y-2.5">
+     <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-x-5 gap-y-2.5">
      {products.map((product) => (
        <ProductCard key={products.id} product={product} />
         ))} 
